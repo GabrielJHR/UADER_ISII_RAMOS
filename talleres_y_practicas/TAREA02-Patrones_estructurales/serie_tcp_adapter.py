d9f82f4d2185e8d16059709b9498f90bce84579d
Gabriel Ramos <gabrieljhramos@gmail.com>
2022-05-05 11:54:11 -0300

    Tarea 2 - ejercicio 2 resuelto

---
+++ talleres_y_practicas/TAREA02-Patrones_estructurales/serie_tcp_adapter.py
@@ -0,0 +1,29 @@
+#*--------------------------------------------------
+#* adapter.py
+#* excerpt from https://refactoring.guru/design-patterns/adapter/python/example
+#*--------------------------------------------------
+
+class Aplicacion:
+    def request(self) -> str:
+        return "La aplicacion tiene que recibir un mensaje desde el servidor."
+
+
+class Servidor:
+    def specific_request(self) -> str:
+        return "Mensaje encriptado: Mensaje desde el servidor"
+
+
+class AdaptadorSerieTCP(Aplicacion, Servidor):
+    def request(self) -> str:
+        return f"Servidor: {self.specific_request()[20:]} "
+
+if __name__ == "__main__":
+    print("Llamando al servidor directamente: ")
+    servidor = Servidor()
+    print(servidor.specific_request())
+
+    print()
+
+    print("Llamado al servidor usando el adapter: ")
+    servidor = AdaptadorSerieTCP()
+    print(servidor.request())
