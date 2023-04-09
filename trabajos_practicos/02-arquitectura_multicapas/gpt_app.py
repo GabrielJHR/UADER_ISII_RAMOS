6e81e76598a8a467f61992ef562c9da4ebca0237
Gabriel Ramos <gabrieljhramos@gmail.com>
2023-04-09 00:59:43 -0300

    Se le permite al usuario enviar sus prompts

--- trabajos_practicos/02-arquitectura_multicapas/gpt_app.py
+++ trabajos_practicos/02-arquitectura_multicapas/gpt_app.py
@@ -2,7 +2,7 @@
 import openai
 
 #……. [ mas Código de inicialización aqui ] …………
-openai.api_key = "---- OPEN AI API_KEY ---"
+openai.api_key = ""
 TOP_P = 1
 FREQ_PENALTY = 0
 PRES_PENALTY = 0
@@ -14,14 +14,20 @@ MODEL_ENGINE = "text-davinci-003"
 
 #…..[otra lógica necesaria – el texto del prompt debe colocarse en userText]…..
 # Set up the model and prompt
-completion = openai.Completion.create(
-    engine=MODEL_ENGINE,
-    prompt=userText,
-    max_tokens=MAX_TOKENS,
-    n=NMAX,
-    top_p=TOP_P,
-    frequency_penalty=FREQ_PENALTY,
-    presence_penalty=PRES_PENALTY,
-    temperature=TEMPERATURE,
-    stop=STOP
-)
+
+userText = input("You: ")
+
+if(userText):
+    completion = openai.Completion.create(
+        engine=MODEL_ENGINE,
+        prompt=userText,
+        max_tokens=MAX_TOKENS,
+        n=NMAX,
+        top_p=TOP_P,
+        frequency_penalty=FREQ_PENALTY,
+        presence_penalty=PRES_PENALTY,
+        temperature=TEMPERATURE,
+        stop=STOP
+    )
+
+    print('ChatGPT: ', completion.choices[0].text)
