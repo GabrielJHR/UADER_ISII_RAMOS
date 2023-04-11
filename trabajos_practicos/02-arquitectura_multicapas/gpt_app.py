89763598141cc0b0f4ff920b601aea0665ddf7d9
Gabriel Ramos <gabrieljhramos@gmail.com>
2023-04-10 23:24:55 -0300

    Se agrego la posibilidad tomar como contexto los resultados previos

--- trabajos_practicos/02-arquitectura_multicapas/gpt_app.py
+++ trabajos_practicos/02-arquitectura_multicapas/gpt_app.py
@@ -27,47 +27,68 @@ MODEL_ENGINE = "text-davinci-003"
 
 # Se define el buffer para almacenar tanto las preguntas como las respuestas
 # dadas por ChatGPT
+buffer = ""
 
 try:
     while(True):
         #.....[otra lógica necesaria – el texto del prompt debe colocarse en userText]…..
         # Set up the model and prompt
+        # Se obtiene la entrada del usuario y se asigna a la variable userText
         userText = input("You: ")
 
+        # Si la entrada del usuario no está vacía, se procesa.
         if(userText):
             try:
-                try:
-                    completion = openai.Completion.create(
-                        engine=MODEL_ENGINE,
-                        prompt=userText,
-                        max_tokens=MAX_TOKENS,
-                        n=NMAX,
-                        top_p=TOP_P,
-                        frequency_penalty=FREQ_PENALTY,
-                        presence_penalty=PRES_PENALTY,
-                        temperature=TEMPERATURE,
-                        stop=STOP
-                    )
+                # Se verifica si el usuario o ChatGPT ha indicado que la conversación está en curso
+                if("--convers" in buffer or "--convers" in userText):
+                    # Si se indica que la conversación está en curso, se agrega la entrada del usuario
+                    # al buffer y se muestra la respuesta de ChatGPT
+                    buffer+=f"You: {userText}\n"
+                else:
+                    # Si no se indica que la conversación está en curso, se sobrescribe el contenido del buffer
+                    # con la entrada del usuario actual
+                    buffer = userText
 
-                    response = completion.choices[0].text
-                    print('ChatGPT: ', response)
+                # Se envía una solicitud a OpenAI para obtener la respuesta del modelo.
+                # Se utiliza la API Key que se encuentra en el archivo "api_key"
+                completion = openai.Completion.create(
+                    engine=MODEL_ENGINE,
+                    prompt=buffer,
+                    max_tokens=MAX_TOKENS,
+                    n=NMAX,
+                    top_p=TOP_P,
+                    frequency_penalty=FREQ_PENALTY,
+                    presence_penalty=PRES_PENALTY,
+                    temperature=TEMPERATURE,
+                    stop=["You:","chatGPT:"]
+                )
 
-                    # Se agrega la pregunta anterior al historial
-                    readline.add_history(userText)
-                except openai.error.AuthenticationError:
-                    print("Error de autenticación: verifique su clave de API de OpenAI.")
-                except openai.error.APIConnectionError:
-                    print("Error de conexión: no se pudo conectar a la API de OpenAI.")
-                except openai.error.APIError:
-                    print("Error de API: se produjo un error al procesar la solicitud.")
-                except openai.error.RateLimitError:
-                    print("Error de límite de tasa: se alcanzó el límite de solicitudes a la API de OpenAI.")
-                except Exception as e:
-                    print("Error: ", e)
+                # Se obtiene la respuesta del modelo y se almacena en la variable "response"
+                response = completion.choices[0].text
 
+                # Se muestra la respuesta del modelo en la consola
+                print(f"chatGPT: {response}\n\n")
+
+                # Se agrega la respuesta del modelo al buffer
+                buffer+=f"chatGPT: {response}\n\n"
+
+                # Se agrega la pregunta anterior del usuario al historial de readline
+                readline.add_history(userText)
+                
+            # Se manejan posibles errores que pueden ocurrir al interactuar con la API de OpenAI
+            except openai.error.AuthenticationError:
+                print("Error de autenticación: verifique su clave de API de OpenAI.")
+            except openai.error.APIConnectionError:
+                print("Error de conexión: no se pudo conectar a la API de OpenAI.")
+            except openai.error.APIError:
+                print("Error de API: se produjo un error al procesar la solicitud.")
+            except openai.error.RateLimitError:
+                print("Error de límite de tasa: se alcanzó el límite de solicitudes a la API de OpenAI.")
             except Exception as e:
-                print("Error al procesar la entrada del usuario: ", e)
+                print("Error: ", e)
+
         else:
+            # Si la entrada del usuario está vacía, se muestra un mensaje en la consola.
             print("La entrada esta vacia")
 except KeyboardInterrupt:
     print("\nInterrupción de teclado detectada. Saliendo del programa...")
