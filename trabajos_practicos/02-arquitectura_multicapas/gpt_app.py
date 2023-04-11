e6bd9e9e25e60a07fdea80bba9e24e412cc37d35
Gabriel Ramos <gabrieljhramos@gmail.com>
2023-04-11 00:24:19 -0300

    Se corrigio el codigo con pylint

--- trabajos_practicos/02-arquitectura_multicapas/gpt_app.py
+++ trabajos_practicos/02-arquitectura_multicapas/gpt_app.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python
+"""
+Este modulo hace llamadas a la api de ChatGPT
+"""
 import openai
 import pyreadline3
 
@@ -9,11 +11,11 @@ readline.set_history_length(1000)
 # Configura la tecla de navegación de historial
 readline.parse_and_bind("up: history-search-backward")
 
-#……. [ mas Código de inicialización aqui ] …………
+# ……. [ mas Código de inicialización aqui ] …………
 # Se ha puesto la api key generada por open ia en un archivo externo llamado 'apy_key'
 # con el objetivo de que no filtrar la api key en el repositorio publico de Github
 # al hacer un git add se va a ignorar el archivo.
-with open("api_key", "r") as file:
+with open("api_key", "r", encoding='utf-8') as file:
     openai.api_key = file.readline()
 
 TOP_P = 1
@@ -30,23 +32,23 @@ MODEL_ENGINE = "text-davinci-003"
 buffer = ""
 
 try:
-    while(True):
-        #.....[otra lógica necesaria – el texto del prompt debe colocarse en userText]…..
+    while True:
+        # .....[otra lógica necesaria – el texto del prompt debe colocarse en userText]…..
         # Set up the model and prompt
         # Se obtiene la entrada del usuario y se asigna a la variable userText
         userText = input("You: ")
 
         # Si la entrada del usuario no está vacía, se procesa.
-        if(userText):
+        if userText:
             try:
                 # Se verifica si el usuario o ChatGPT ha indicado que la conversación está en curso
-                if("--convers" in buffer or "--convers" in userText):
-                    # Si se indica que la conversación está en curso, se agrega la entrada del usuario
-                    # al buffer y se muestra la respuesta de ChatGPT
-                    buffer+=f"You: {userText}\n"
+                if "--convers" in buffer or "--convers" in userText:
+                    # Si se indica que la conversación está en curso, se agrega la entrada del
+                    # usuario al buffer y se muestra la respuesta de ChatGPT
+                    buffer += f"You: {userText}\n"
                 else:
-                    # Si no se indica que la conversación está en curso, se sobrescribe el contenido del buffer
-                    # con la entrada del usuario actual
+                    # Si no se indica que la conversación está en curso, se sobrescribe el contenido
+                    # del buffer con la entrada del usuario actual
                     buffer = userText
 
                 # Se envía una solicitud a OpenAI para obtener la respuesta del modelo.
@@ -60,7 +62,7 @@ try:
                     frequency_penalty=FREQ_PENALTY,
                     presence_penalty=PRES_PENALTY,
                     temperature=TEMPERATURE,
-                    stop=["You:","chatGPT:"]
+                    stop=["You:", "chatGPT:"]
                 )
 
                 # Se obtiene la respuesta del modelo y se almacena en la variable "response"
@@ -70,11 +72,11 @@ try:
                 print(f"chatGPT: {response}\n\n")
 
                 # Se agrega la respuesta del modelo al buffer
-                buffer+=f"chatGPT: {response}\n\n"
+                buffer += f"chatGPT: {response}\n\n"
 
                 # Se agrega la pregunta anterior del usuario al historial de readline
                 readline.add_history(userText)
-                
+
             # Se manejan posibles errores que pueden ocurrir al interactuar con la API de OpenAI
             except openai.error.AuthenticationError:
                 print("Error de autenticación: verifique su clave de API de OpenAI.")
@@ -83,7 +85,8 @@ try:
             except openai.error.APIError:
                 print("Error de API: se produjo un error al procesar la solicitud.")
             except openai.error.RateLimitError:
-                print("Error de límite de tasa: se alcanzó el límite de solicitudes a la API de OpenAI.")
+                print(
+                    "Error de límite de tasa: se alcanzó el límite de solicitudes a la API de OpenAI.")
             except Exception as e:
                 print("Error: ", e)
 
